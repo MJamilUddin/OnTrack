8b8e5fc60c634b243fdf9c4b6c4fe51a7bf590ed
Jamil Uddin <jamil@Jamils-MacBook-Pro.local>
2023-03-05 16:55:28 +0000

    merge changes resolved

--- src/functions/nameTransfer.js
+++ src/functions/nameTransfer.js
@@ -2,8 +2,12 @@ const nameTransfer = {
     "GCSEMathsAQA": "GCSE Maths AQA", 
     "GCSEBiologyAQA": "GCSE Biology AQA", 
     "GCSEPhysicsAQA": "GCSE Physics AQA", 
+<<<<<<< HEAD
     "GCSEChemistryAQA": "GCSE Chemistry AQA",
     "AlevelMathsAQA": "A-level Maths AQA", 
+=======
+    "GCSEBiologyAQA": "GCSE Physics AQA", 
+>>>>>>> fc37400 (functions change)
     "AlevelPhysicsAQA": "A-level Physics AQA"
 };
 
